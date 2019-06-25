39873f6ee2c2c3adb5eb7f46169788f85c65babb
SetaSensei <sensei.seta@gmail.com>
2019-06-25 17:47:13 +0200

    Add mockito properly

--- java-project/src/test/java/com/yourself/AlarmTest.java
+++ java-project/src/test/java/com/yourself/AlarmTest.java
@@ -1,12 +1,16 @@
 package com.yourself;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
 import org.junit.Test;
 
 public class AlarmTest {
 
     @Test
     public void alarmShouldAlwaysRing() {
+        IClock clock = mock(IClock.class);
+
         Alarm alarm = new Alarm();
         String ring = alarm.check();
 
